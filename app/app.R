e6c53657413979a6bc3707f62863dcc0ab3a2e51
amalirajapaksha <amali2022ugc@gmail.com>
2025-10-25 07:54:46 +0530

    Update app.R

--- app/app.R
+++ app/app.R
@@ -1,1 +1,15 @@
+# Shiny Dashboard for Canada Health Data - Coming Soon
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Canada Health Data Dashboard"),
+  sidebarLayout(
+    sidebarPanel("Sidebar"),
+    mainPanel("Dashboard Visualizations Coming Soon")
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui, server)
 
